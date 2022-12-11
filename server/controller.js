422223668cf7c3541d169bf2aff5e37464591d21
Koreyton Evans <koreyton.evans1995@gmail.com>
2022-12-11 16:45:51 -0600

    changes

--- server/controller.js
+++ server/controller.js
@@ -253,5 +253,13 @@ FROM cities AS a
         `).then((dbRes) =>{
             res.status(200).send(dbRes[0])
          }).catch(err => console.log('error seeding DB', err))
+    }, 
+    deleteCities: (req, res)=>{
+       const {id} = req.parms;
+       sequelize.query(`
+       delete ${id} from cities;
+       `).then((dbRes) =>{
+        res.status(200).send(dbRes[0])
+     }).catch(err => console.log('error seeding DB', err))
     }
 }
